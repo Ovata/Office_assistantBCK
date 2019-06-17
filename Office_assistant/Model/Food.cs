1c2d881eb192b9cf4a113dea238f3422ab9cd857
Victor Atoyebi <tatoyebi@eminenttechnology.com>
2019-06-17 19:23:15 +0100

    First Commit

---
+++ Office_assistant/Model/Food.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Office_assistant.Model
+{
+    public class Food
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Food_name { get; set; }
+        [Required]
+        public int Price { get; set; }
+    }
+
+    
+}
